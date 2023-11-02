69522ebaf0aae53e7bbca0db951fcad2af5d23f9
edharcourt <edharcourt@gmail.com>
2023-11-02 16:27:20 -0400

    Add HW 7

---
+++ src/HW8/Main.java
@@ -0,0 +1,31 @@
+package HW8;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BankAccount ba = new BankAccount("John", "Doe",
+                123456789, "123 Main St", "Anytown", "NY", "12345", 1000.00, 0.05);
+        System.out.println(ba);
+        ba.debit(2000.00);  // take out $2000 (that is 100 too much)
+        ba.credit(100.00);                    // add in 100
+        System.out.println(ba.getBalance());  // print the balance
+        ba.debit(50.00);                  // take out 50
+        System.out.println(ba);               // print the account
+        ba.setInterestRate(0.06);
+        System.out.println(ba);
+        ba.addInterest();
+        System.out.println(ba.getBalance());
+
+        BankAccount ba2 = new BankAccount("Jane", "Doe",
+                987654321, "456 Main St", "Anytown", "NY", "12345", 2000.00, 0.05);
+        System.out.println(ba2);
+        ba2.credit(100.00);
+        System.out.println(ba2.getBalance());
+        ba2.debit(50.00);
+        System.out.println(ba2);
+        ba2.setInterestRate(0.06);
+        System.out.println(ba2);
+        ba2.addInterest();
+        System.out.println(ba2.getBalance());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -6,9 +7,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int x = s.nextInt();
-        String c_or_f = s.nextLine();
-        System.out.println(x + "----" + c_or_f.strip());
+        //Scanner s = new Scanner(System.in);
+        //int x = s.nextInt();
+        //String c_or_f = s.nextLine();
+        //System.out.println(x + "----" + c_or_f.strip());
+        System.out.println(Math.floorMod(-76,11));
+        System.out.println("banana".compareTo("apple") > 0);
+        System.out.println("Hermione said, \"Don't \\forget\\ to say \"expelliarmus\"");
+        ArrayList<Integer> aloi = new ArrayList<>();
+        aloi.add(6);
+        System.out.println(aloi);
+        aloi.remove(aloi.size() - 1);
+        aloi.set(aloi.size() - 1, aloi.get(aloi.size() - 1) + 2);
+        System.out.println(aloi);
+        System.out.println("Hermione said, \"Don't \\forget\\ to say \"expelliarmus\".");
     }
 }
